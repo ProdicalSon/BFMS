5f842edd584057c031ec1ddcb6e1f7664cc9f50b
alvin <wesealv@gmail.com>
2024-07-02 10:30:19 +0300

    here comes the user redesigned😂

--- private_user/dashboard/db.php
+++ private_user/dashboard/db.php
@@ -1,8 +1,8 @@
 <?php 
 $host = "localhost";
 $username = "root";
-$password = "";
-$database = "bfms";
+$password = "wesel123@90";
+$database = "bungoma_filing_system_DB";
 
 $conn = mysqli_connect($host,$username,$password,$database);
 ?>
