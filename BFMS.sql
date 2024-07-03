547591b2d902879b953e81922714c578b723bd8a
ProdicalSon <siundubrian65@gmail.com>
2024-07-03 10:08:28 +0300

    U

--- BFMS.sql
+++ BFMS.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 27, 2021 at 04:02 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.3.27
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+03:00";
@@ -18,166 +9,41 @@ SET time_zone = "+03:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `BFMS`
+
 --
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `admin_login`
---
-
-CREATE TABLE `file_category` (
-  `category_id` int(11) NOT NULL,
-  `category_Name` text NOT NULL,
-  `category_description` text NOT NULL,
-  `color-label` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `admin_login`
---
-
-INSERT INTO `file_category` (`category_id`, `category_Name`, `category_description`, `label`) VALUES
-(
-
-);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `register`
---
-
-CREATE TABLE `register` (
-  `firstname` varchar(50) NOT NULL,
-  `lastname` varchar(50) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `id_number` int(10) NOT NULL,
-  `employ_number` varchar(20) NOT NULL,
-  `email` text NOT NULL,
-  `passwordd` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `register`
---
-
-INSERT INTO `register` (`firstname`, `lastname`, `username`, `id_number`, `employ_number`, `email`, `passwordd`) VALUES
-('john', 'brian', 'johnbrian', '39694411', '11449', 'siundubrian65@gmail.com', 'ruthsirengo123');
 
 
--- --------------------------------------------------------
-
---
--- Table structure for table `history_log1`
---
-
-CREATE TABLE `history_log1` (
-  `log_id` int(11) NOT NULL,
-  `id_number` int(11) NOT NULL,
-  `admin_user` text NOT NULL,
-  `action` varchar(100) NOT NULL,
-  `actions` varchar(200) NOT NULL DEFAULT 'Has LoggedOut the system at',
-  `ip` text NOT NULL,
-  `host` text NOT NULL,
-  `login_time` varchar(200) NOT NULL,
-  `logout_time` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `history_log1`
---
-
-INSERT INTO `history_log1` (
-
-);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `login_user`
---
-
-CREATE TABLE `login_user` (
-  `id_number` int(11) NOT NULL,
-  `username` text NOT NULL,
-  `password` text NOT NULL,
-  
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `upload_files`
---
-
-CREATE TABLE files (
+-- Create the users table
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    filename VARCHAR(255) NOT NULL,
-    originalname VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL,
-    path VARCHAR(255) NOT NULL,
-    mimetype VARCHAR(255) NOT NULL,
-    size INT NOT NULL,
-    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    other_name VARCHAR(50),
+    employee_number VARCHAR(50),
+    department_description VARCHAR(100),
+    id_no INT NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
-
---
--- Dumping data for table `files`
---
-
-INSERT INTO `files` (`id`, `filename`, `originalname`, `CATEGORY`, `path`, 'mimetype', `size`, 'uploaded_at') VALUES
-(
-
+-- Create the login_attempts table
+CREATE TABLE login_attempts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    attempt_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    success BOOLEAN,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin_login`
---
-ALTER TABLE `admin_login`
-  ADD PRIMARY KEY (`id_number`);
-
---
--- Indexes for table `login_user`
---
-ALTER TABLE `login_user`
-  ADD PRIMARY KEY (`id_number`);
-
---
--- Indexes for table `upload_files`
---
-ALTER TABLE `files`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin_login`
---
-ALTER TABLE `admin_login`
-  MODIFY `id_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT for table `login_user`
---
-ALTER TABLE `login_user`
-  MODIFY `id_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `upload_files`
---
-ALTER TABLE `files`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Create the password_reset_tokens table
+CREATE TABLE password_reset_tokens (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    token VARCHAR(6) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expires_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
